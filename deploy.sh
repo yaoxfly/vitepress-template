c110b16322f892abe21e9fcb01e6e09649190cfc
yaoxfly <1315892088@qq.com>
2022-09-19 17:44:30 +0800

    feat:添加自动部署配置

---
+++ deploy.sh
@@ -0,0 +1,28 @@
+#!/usr/bin/env sh
+
+# 忽略错误
+set -e
+
+# 构建
+npm run docs:build
+
+# 进入待发布的目录
+cd docs/.vitepress/dist
+
+# 如果是发布到自定义域名
+# echo 'www.example.com' > CNAME
+
+git init
+git add -A
+git commit -m 'deploy'
+
+# 如果部署到 https://<USERNAME>.github.io
+# git push -f git@github.com:<USERNAME>/<USERNAME>.github.io.git master
+
+# 如果是部署到 https://<USERNAME>.github.io/<REPO>
+# git push -f git@github.com:<USERNAME>/<REPO>.git master:gh-pages
+
+
+git push -f https://github.com/yaoxfly/vitepress-template-site.git master:main
+
+cd -
